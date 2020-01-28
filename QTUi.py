31f36f3ec09e09b89f838dfaa6b027c34aa80a90
Administrator <847337934@qq.com>
2020-01-28 16:14:03 +0800

    123

---
+++ QTUi.py
@@ -0,0 +1,16 @@
+from QTtest import Ui_MainWindow
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+
+class CamShow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(CamShow, self).__init__(parent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = CamShow()
+    ui.show()
+    sys.exit(app.exec_())
